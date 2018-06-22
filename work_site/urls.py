6315ffaa9e788145f29fbb980b3473179f19f13a
lychlov <czk499658904@126.com>
2018-06-22 16:29:44 +0800

    useful

--- work_site/urls.py
+++ work_site/urls.py
@@ -24,6 +24,5 @@ from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('crawler/', include('crawler.urls')),
                   path('chenlin/', include('chenlin.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
